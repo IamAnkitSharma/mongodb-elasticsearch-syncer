005f05426105a5fa98b0d1b70722942e7a4059d9
Ankit Sharma <sankit945@gmail.com>
2022-05-08 18:25:08 +0530

    formatted some code

--- job.model.js
+++ job.model.js
@@ -1,24 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 // import mongoosastic from "mongoosastic";
 
-const jobSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const jobSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
   },
-  description : {
-    type: String,
-    required: true
-  }
-}, {
+  {
     timestamps: true,
-});
+  }
+);
 
 // jobSchema.plugin(mongoosastic, {
 //   hosts: ["localhost:9200"],
 //   index: "jobs",
 // });
 
-const Job = mongoose.model("Job", jobSchema);
+const Job = mongoose.model('Job', jobSchema);
 
 export { Job };
